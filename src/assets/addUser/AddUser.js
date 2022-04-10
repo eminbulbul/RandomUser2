579c9aac6bd42ce31616453c8ba71f3505d85417
m_emin <mehmeteminbul@gmail.com>
2022-04-11 01:49:39 +0300

    add user table added

--- src/assets/addUser/AddUser.js
+++ src/assets/addUser/AddUser.js
@@ -3,7 +3,20 @@ import React from "react";
 const AddUser = ({ card }) => {
   return (
     <div>
-      <table></table>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>eMail</th>
+          <th>Phone</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+          <td>Germany</td>
+        </tr>
+      </table>
     </div>
   );
 };
